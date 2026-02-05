dda86e55c726cf4e71f69fcdefc761936ff0d1c7
muhajirpayao <muhajir.payao@kmc.solutions>
2026-02-05 17:24:41 +0800

    check_online

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwdindcss from '@tailwindcss/vite'
+
+// i dont know what to do any more
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwdindcss()],
