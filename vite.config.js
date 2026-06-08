e929a54c5c52def52de4a18b9c417741858b960d
Muhajir Payao <YOUR_GITHUB_EMAIL>
2026-06-08 20:29:06 +0800

    fix vercel base path

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwdindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwdindcss()],
-  base: '/portfolio-react/'
+  plugins: [react(), tailwdindcss()]
 })
